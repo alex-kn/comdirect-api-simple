d9f37642daec0695bc279d2775978cb42a4599ee
Alexander Knittel <alx.kntl@gmail.com>
2020-07-26 15:36:31 +0200

    Add setup.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# Comdirect API
+
+This is a python wrapper for the comdirect API for private consumers (April 2020).
+
+This package currently only supports read operations, which include:
+
+TODO
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="comdirect-api-simple",
+    version="0.0.1",
+    author="Alexander Knittel",
+    author_email="alx.kntl@gmail.com",
+    description="A package for read operations for the comdirect API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex-kn/comdirect-api-simple",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
