c710e9bf5751ece4ac8f657f02463ae4329fe98c
Kontiko <kontiko.fb@gmail.com>
2020-11-24 12:40:49 +0100

    added paging count to account transactions
    paging count is the number of transaction which get returned

--- comdirect_api/service/account_service.py
+++ comdirect_api/service/account_service.py
@@ -27,17 +27,20 @@ class AccountService:
         response = self.session.get(url).json()
         return response
 
-    def get_account_transactions(self, account_uuid, with_account=False, transaction_state='BOTH'):
+    def get_account_transactions(self, account_uuid,paging_count=20, with_account=False, transaction_state='BOTH'):
         """
         4.1.3. Fetch transactions for a specific account.
 
         :param account_uuid:  Account-ID
+        :paging_count: Number of transactions
         :param with_account: Include account information in the response. Defaults to False
         :param transaction_state: 'BOOKED' or 'NOTBOOKED'. Defaults to 'BOTH'
         :return: Response object
         """
         url = '{0}/banking/v1/accounts/{1}/transactions'.format(self.api_url, account_uuid)
-        params = {'transactionState': transaction_state}
+        params = {'transactionState': transaction_state,
+                  'paging-count':paging_count
+                  }
         if with_account:
             params['with-attr'] = 'account'
 
